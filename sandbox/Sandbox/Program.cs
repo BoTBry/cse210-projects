f5bb2cba72341cac70ff70579b2da5023e229427
BoTBry <abcchigbu@gmail.com>
2023-05-07 19:15:11 +0100

    completed C# programming parts 1-2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,6 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Hello Chigbu Bryan,");
-        Console.Writeline("I would love to ask yo some questions.")
+        Console.WriteLine("I would like to ask you some questions");
+
+        Console.WriteLine("");
+            Console.Write("what is your First Name? ");
+            string frist_name = Console.ReadLine();
+
+        Console.WriteLine("");
+            Console.Write("what is your Second Name? ");
+        string second_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {frist_name}, {second_name} {frist_name}.");
+
     }
 }
